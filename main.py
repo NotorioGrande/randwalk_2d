599ec62c98a710efad13b98fbb91ac1e57983cc4
Gustavo Bhering Grande <gustavobgrande@hotmail.com>
2022-09-25 19:41:39 -0300

    inicial

---
+++ main.py
@@ -0,0 +1,59 @@
+import random
+# classe da particula
+class Walker:
+    #alpha é alpha
+    #size é o L
+    def __init__(self, alpha, size):
+        self.alpha = alpha
+        self.size = size
+        #array das probabilidades
+        #p_array[0] = cima
+        #p_array[1] = direita
+        #p_array[2] = baixo
+        #p_array[3] = esquerda
+        self.p_array = [0.25, 0.25, 0.25, 0.25]
+        #variavel que controla o tempo
+        self.time = 0
+        self.pos = [0, 0]
+    
+    def walk(self):
+        rand_num = random.random()
+        dir = 0
+        
+        #acumulativa
+        cum = 0
+        for k, v in enumerate(self.p_array):
+            cum += v
+            if rand_num <= cum:
+                dir = k
+                break
+        # atualiza posicao
+        if(k == 0):
+            self.pos[1]+=1
+        elif(k==1):
+            self.pos[0]+=1
+        elif(k==2):
+            self.pos[1]-=1
+        else:
+            self.pos[0]-=1
+        
+
+        #atualizar o array de probabilidade
+        for k, v in enumerate(self.p_array):
+            if k == dir:
+                self.p_array[k] = 0.25 + self.alpha
+            ##direcao oposta
+            elif (k + 2)%4:
+                self.p_array[k] = 0.25 - self.alpha
+            else:
+                self.p_array[k] = 0.25
+        self.time+=1
+    #  retorna o array [x, y, tempo]
+    def simulate(self):
+        while (abs(self.pos[0]) != self.size and abs(self.pos[1]) != self.size):
+            self.walk()
+        return [self.pos[0], self.pos[1], self.time]
+
+
+a1 = Walker(0.25, 10)
+print(a1.simulate())
